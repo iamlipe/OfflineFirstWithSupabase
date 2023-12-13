f93326145122ad11dde4765f63266ee0e77a8893
Felipe Lima <felipe_lima10@icloud.com>
2023-12-12 23:23:32 -0300

    Update profile-model.ts

--- src/models/profile-model.ts
+++ src/models/profile-model.ts
@@ -5,7 +5,6 @@ import { date, readonly, text } from "@nozbe/watermelondb/decorators";
 export class Profile extends Model {
     static table = "profiles";
     @text("name") name!: string;
-    @text("website") website!: string;
     @readonly @date("created_at") createdAt!: Date;
     @readonly @date("updated_at") updatedAt!: Date;
 }
